e675cea6ae1621d55441f1dd055c9892960a511d
huafeng_shi <huafeng_shi@tianma.cn>
2026-01-12 16:01:06 +0800

    测试count300未卡死，中间模拟断开5次

--- stm32/nexlink_template/Middlewares/ST/STM32_USB_Device_Library/Class/NEX_LINK/Src/nexlink_tx.c
+++ stm32/nexlink_template/Middlewares/ST/STM32_USB_Device_Library/Class/NEX_LINK/Src/nexlink_tx.c
@@ -5,7 +5,6 @@
 #include "queue.h"
 #include "usbd_def.h"
 #include <string.h>
-#include "usbd_nex_link.h"
 
 #define TX_MAX_LEN 256
 #define TX_QUEUE_LEN 8
@@ -35,14 +34,12 @@ void nexlink_tx_send(const void *data, uint16_t len)
     xQueueSend(txq, &item, portMAX_DELAY);
 }
 
-extern USBD_HandleTypeDef hUSB;
 void NexLinkTxTask(void *arg)
 {
     tx_item_t item;
     for (;;)
     {
-        if (USBD_NEX_LINK_TxReady(&hUSB))
-            if (xQueueReceive(txq, &item, portMAX_DELAY))
-                usb_tx(item.buf, item.len);
+			if (xQueueReceive(txq, &item, portMAX_DELAY))
+					usb_tx(item.buf, item.len);
     }
 }
